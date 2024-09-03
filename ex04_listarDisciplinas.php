9eb9cc95623f980696324abe5d0da67f56c91f05
zAlanRibeiro <alandoyle1903@gmail.com>
2024-09-03 15:29:49 -0300

    Adicionar disciplinas

---
+++ ex04_listarDisciplinas.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>Listar Disciplina</h1>
+<table>
+   <tr>
+       <th>Nome</th>
+       <th>Sigal</th>
+       <th>Carga Horária</th>
+   </tr>
+   <?php
+       $arquivo = fopen("disciplinas.txt","r") or die ("Erro ao abrir o arquivo!");
+       $cont = 0;
+       $linha = fgets($arquivo);
+       while (!feof($arquivo)){
+           $linha = fgets($arquivo);
+           $colunaDados = explode(";",$linha);
+           echo "<tr>";
+           echo "<td>$colunaDados[0]</td>";
+           echo "<td>$colunaDados[1]</td>";
+           echo "<td>$colunaDados[2]</td>";
+           echo "<tr>";
+       }
+       fclose($arquivo)
+   ?>  
+</table>
+</body>
+</html>
+
