29af70f6b504fc7b36629bbbf142388da8c8bc9b
Your Name <you@example.com>
2024-09-17 10:57:22 -0300

    edit

---
+++ Teste/ex05_alterarD.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+   <head>    
+   </head>
+   <body>
+       <h1>Editar Disciplina</h1>
+       <form action="ex05_alterarD2.php" action="ex05_alterarD3.php" method="POST">
+       Qual disciplina deseja editar:
+       <input type="text" name="materia">
+       <input type="submit" value="Ok" href="ex05_alterarD2.php">
+   </body>
+</html>
