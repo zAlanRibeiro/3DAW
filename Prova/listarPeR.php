dd3f4f7c3012343ecb8ea730ca0311b7dcd43402
Alan Doyle Costa Ribeiro <142350335+zAlanRibeiro@users.noreply.github.com>
2024-09-24 18:10:31 -0300

    Update listarPeR.php

--- Prova/listarPeR.php
+++ Prova/listarPeR.php
@@ -23,7 +23,7 @@
         <table class="minha-tabela">
             <thead>
                 <tr>
-                    <th>N da Perguna</th>
+                    <th>N da Pergunta</th>
                     <th>Pergunta</th>
                     <th>Resposta 1</th>
                     <th>Resposta 2</th>
@@ -32,30 +32,31 @@
                     <th>Resposta Correta</th>
                 </tr>
             </thead>
+            <tbody>
+                <?php
+                    $arquivo = fopen("PerguntasErespostas.txt", "r") or die ("Erro ao abrir o arquivo!");
+                    $primeiraLinha = true;
+                    while (!feof($arquivo)) {
+                        $linha = fgets($arquivo);
+                        if ($primeiraLinha) {
+                            $primeiraLinha = false; 
+                            continue;
+                        }
+                            $colunaDados = explode(";", $linha);
+                            echo "<tr>";
+                            echo "<td>" . htmlspecialchars($colunaDados[0]) . "</td>";
+                            echo "<td>" . htmlspecialchars($colunaDados[1]) . "</td>";
+                            echo "<td>" . htmlspecialchars($colunaDados[2]) . "</td>";
+                            echo "<td>" . htmlspecialchars($colunaDados[3]) . "</td>";
+                            echo "<td>" . htmlspecialchars($colunaDados[4]) . "</td>";
+                            echo "<td>" . htmlspecialchars($colunaDados[5]) . "</td>";
+                            echo "<td>" . htmlspecialchars($colunaDados[6]) . "</td>";
+                            echo "</tr>";   
+                    }
+                    fclose($arquivo);
+                ?>
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
-
-<?php
-    $arquivo = fopen("PerguntasErespostas.txt", "r") or die ("Erro ao abrir o arquivo!");
-    $primeiraLinha = true;
-    while (!feof($arquivo)) {
-        $linha = fgets($arquivo);
-        if ($primeiraLinha) {
-            $primeiraLinha = false; 
-            continue;
-        }
-        $colunaDados = explode(";", $linha);
-        echo "<tr>";
-        echo "<td>{$colunaDados[0]}</td>";
-        echo "<td>{$colunaDados[1]}</td>";
-        echo "<td>{$colunaDados[2]}</td>";
-        echo "<td>{$colunaDados[3]}</td>";
-        echo "<td>{$colunaDados[4]}</td>";
-        echo "<td>{$colunaDados[5]}</td>";
-        echo "<td>{$colunaDados[6]}</td>";
-        echo "</tr>"; 
-    }
-    fclose($arquivo);
-?>  
-</table>
